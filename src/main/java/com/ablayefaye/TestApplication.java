4dd12efbbbf8b349b6383f231085dd4e3ea76c9e
ablayefayenevrose <layefayenevrose@gmail.com>
2023-01-12 17:12:54 +0000

    first commit

---
+++ src/main/java/com/ablayefaye/TestApplication.java
@@ -0,0 +1,23 @@
+package com.ablayefaye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+
+	@GetMapping("/sayhello")
+	public String SayHello(){
+		return "hello This is realy Working";
+	}
+}
